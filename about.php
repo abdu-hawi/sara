2ec5d96746b4f16c186c7d7515b1c26aa8ea06c3
abdu-hawi <ahhh42@gmail.com>
2022-03-08 16:22:54 +0300

    0

---
+++ about.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="rtl">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="about.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="header-bg"></div>
+<img src="logo.png" width="147" height="147" class="logo" />
+
+<ul class="menu">
+ <li class="main-menu"><a href="#">الرئيسية</a></li>
+ <li class="main-menu"><a href="#">الدليل الزراعي</a></li>
+ <li class="main-menu">
+ 	<a href="javascript:void(0)" class="dropbtn">سجل الطلبات</a>
+    <div class="dropdown-content">
+      <a href="record.html">الطلبات الحالية</a>
+      <a href="#">الطلبات السابقة</a>
+    </div>
+ </li>
+ <li class="main-menu"><a href="#">من نحن</a></li>
+ <li class="main-menu"><a href="#">خدماتنا</a></li>
+ <li class="main-menu"><a href="#">تواصل معنا</a></li>
+</ul>
+
+<ul class="left">
+	<li class="icon-menu"><a href="#"><img src="user.png" width="25" height="25" /></a></li>
+    <li class="icon-menu"><a href="#"><img src="notify.png" width="25" height="25" /></a></li>
+    <li class="icon-menu"><a href="#"><img src="cart.png" width="25" height="25" /></a></li>
+</ul>
+
+
+<div class="content">
+    <div class="farm-desc">
+    	<h1 style="color:#000;">مختصون الزراعة</h1>
+    	<h3>مختص الزراعة هو المسؤول عن العناية والاهتمام بالنباتات و عمليات الري و وضع الاسمدة ومراقبة عملية الزراعة والاشراف عليها</h4>
+    	<h3>- توفر لك هذه الخدمة التواصل السريع واختيار مختص الزراعة الخاص بك -</h3>
+    </div>
+    <img src="farm.png" width="200" height="200" />
+    
+</div>
+<div class="bg-items">
+	<div class="content-items">
+    	<div class="cont">
+        	<div style="display:inline-flex;">
+                <div class="input-search">
+                    <input type="search" placeholder="Find a member..." class="search"  />
+                    <img src="search.png" class="img-search"  />
+                    <span class="number-cnt">10</span>
+                </div>
+                <div class="list-view">
+                    <a href="#">
+                        <img src="list-view.png" width="20" height="20" title="List view" style="margin-top: 5px;" /> 
+                    </a>
+                </div>
+                <div class="grid-view">
+                    <a href="#">
+                        <img src="grid-view.png" width="20" height="20" title="Grid view" style="margin-top: 5px;" />
+                    </a>
+                </div> 
+            </div>
+            <div class="head-left">
+                <select>
+                    <option>All members</option>
+                    <option>Collobrators</option>
+                    <option>Writers</option>
+                </select>
+                <label for="dp">Sort by:
+                    <select id="dp">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                    </select>
+                </label>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="btn-chat">
+<img src="chat.png" width="40" height="40" /> </div>
+
+</body>
+</html>
